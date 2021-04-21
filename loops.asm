ad43dbc1a58ee3101bfdcf82f284637053d8be6a
Yaowenqiang <yaowenqiang111@163.com>
2021-04-21 22:19:54 +0800

    add loops.asm

---
+++ loops.asm
@@ -0,0 +1,25 @@
+section .text
+	global main
+main:
+	mov ecx, 10
+	mov eax, '1'
+l1:
+	mov [num], eax
+	mov eax, 4
+	push ecx
+
+	mov ecx, num
+	mov edx ,1
+	int 0x80
+
+	mov eax, [num]
+	sub eax, '0'
+	inc eax
+	add eax, '0'
+	pop ecx
+	loop l1
+
+	mov eax, 1
+	int 0x80
+section .bss
+num resb 1
