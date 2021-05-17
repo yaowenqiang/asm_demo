9365a850723600d2671521e2536618fce3779406
Yaowenqiang <yaowenqiang111@163.com>
2021-05-17 22:07:36 +0800

    add AddTwo.asm

---
+++ AddTwo.asm
@@ -0,0 +1,15 @@
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode:DWORD
+.data
+    sum DWORD 0
+
+.code
+main PROC
+    mov eax, 5
+    add eax, 6
+    
+    INVOKE ExitProcess, 0
+main ENDP
+END main
